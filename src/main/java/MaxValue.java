bf76b692ee43bab4594717d8b28bc645b59afc29
Maryna Osadchuk <maryna.osadchuk@infopulse.com>
2015-03-10 20:23:04 +0200

    MaxValue class was created

---
+++ src/main/java/MaxValue.java
@@ -0,0 +1,15 @@
+/**
+ * Created by Maryna.Osadchuk on 3/10/2015.
+ */
+public class MaxValue {
+    public static void main(String[] args) {
+        int x = 678;
+        int y = 35;
+        int maxValue = 0;
+        if (x > y)
+        {
+            maxValue = x;
+        } else maxValue = y;
+        System.out.println("Maximal Value is " + maxValue);
+    }
+}
